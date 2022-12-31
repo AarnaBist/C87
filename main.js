b2203183fdd64bf8c0bc8b3471d375277bcc6e48
AarnaBist <114805579+AarnaBist@users.noreply.github.com>
2022-12-31 16:52:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,20 @@ function player_update()
         canvas.add(player_object);
     });
 }
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img) {
+        block_image_object= Img;
+
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_image_object);
+    });
+}
 
 window.addEventListener("keydown", my_keydown);
 
@@ -99,12 +113,12 @@ function my_keydown(e)
    }
    if(keyPressed == '68')
    {
-    new_image('dark_green.jpg');
+    new_image('dark_green.png');
     console.log("d");
    }
    if(keyPressed == '85')
    {
-    new_image('different.png');
+    new_image('unique.png');
     console.log("u");
    }
    if(keyPressed == '89')
